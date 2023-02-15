59037208a0c39dda56b1a52f448b52e4da0e0c87
Junyoung Kim <jkim5929@gmail.com>
2023-02-14 22:52:37 -0500

    First Commit, got movie search working. Working on movie night

---
+++ Models/OMDB_DAL.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Searching_OMDB_Lab.Models
+{
+    public class OMDB_DAL
+    {
+        public static OMDB_Model GetResponse(string title)
+        {
+            //setup
+            string key = "855b96ef";
+            string url = $"http://www.omdbapi.com/?apikey={key}&t={title}";
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); //casting
+
+            //convert to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //c#
+            OMDB_Model result = JsonConvert.DeserializeObject<OMDB_Model>(JSON);
+            return result;
+
+
+
+        }
+    }
+}
